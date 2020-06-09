e9912d2513c610e1fb8db754731196a78c29f5e4
Dwi Asmara Putra <putra.work@gmail.com>
2020-06-09 11:18:13 +0800

    commit aja

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <!-- Header-->
+    <header>
+      <h1>I Made Dwi Asmara Putra</h1>
+      <p>Web Programmer</p>
+    </header>
+
+    <div class="container">
+		
+      <!-- Introduction-->
+      <section class="center">
+        <h1>Hello World!</h1>
+        <p>I am a Web Developer from Bali. I started my career as a programmer in 2013 after I graduated from STIKOM BALI.</p>
+      </section>
+
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
